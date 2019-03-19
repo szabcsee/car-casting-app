bba7bea72b99c388b577deb175f7524e5113ee35
Szabolcs Bokonyi <sbokonyi@sicom.com>
2019-03-19 18:23:55 +0100

    Playing with basic angular setup

--- angular-frontend/src/app/app.module.ts
+++ angular-frontend/src/app/app.module.ts
@@ -1,18 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {NgModule} from '@angular/core'; 
+import {BrowserModule} from '@angular/platform-browser';
+import { HomepageComponent} from './homepage.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	imports: [
+		BrowserModule
+	],
+	declarations: [
+		HomepageComponent
+	],
+	bootstrap: [
+		HomepageComponent
+	]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ angular-frontend/src/app/homepage.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'main-app',
+	template: '<h1>Freelance Bootcamp Homepage2</h1>'
+})
+export class HomepageComponent {}
