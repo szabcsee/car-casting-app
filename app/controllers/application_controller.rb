eebc77abe8f076a3fd455fa53e2c7f3c8a0e66f2
Szabolcs Bokonyi <sbokonyi@sicom.com>
2019-06-28 12:14:16 +0200

    Admin added to migration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,12 @@
 class ApplicationController < ActionController::Base
   before_filter :set_gettext_locale
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:admin])
+  end
 end
