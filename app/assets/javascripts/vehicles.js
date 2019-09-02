a90115d49277fb9c77c5d2eb5a9041647dfb2ae2
Szabolcs Bokonyi <sbokonyi@sicom.com>
2019-09-02 10:25:02 +0200

    Reorganizing vehicle form and adding style

--- app/assets/javascripts/vehicles.js
+++ app/assets/javascripts/vehicles.js
@@ -1,30 +1,34 @@
-$(document).ready(function(){
-   $('.vehicle_brand_select').change(function(event){
-       let field = this;
-       if (field.value.length >= 3) {
-           $.ajax({
-               url: '/vehicle_models_by_brand',
-               dataType: 'json',
-               type: 'GET',
-               data: {"vehicle_brand_id": this.value, "vehicle_type_id": $("#vehicle_vehicle_type_id")[0].value},
-               success: function(response) {
-                   let array = response.model_data,
-                       tabId = "#vehicle_type_" + response.vehicle_type_id,
-                       modelSelect = $(tabId).find('.vehicle_model_select')[0],
-                       modelDataList = $(tabId).find('#vehicle_model')[0];
-                   $(modelSelect).prop('disabled', false);
-                   if (array.length > 0) {
-                       for(i in array) {
-                           $(modelDataList).append("<option value='"+array[i]['name']+"'>"+array[i]['name']+"</option>");
-                       }
-                   }
+$(document).ready(function () {
+    $('.vehicle_brand_select').change(function (event) {
+        let field = this;
+        if (field.value.length >= 3) {
+            $.ajax({
+                url: '/vehicle_models_by_brand',
+                dataType: 'json',
+                type: 'GET',
+                data: {"vehicle_brand_id": this.value, "vehicle_type_id": $("#vehicle_vehicle_type_id")[0].value},
+                success: function (response) {
+                    let modelData = response.model_data,
+                        tabId = "#vehicle_type_" + response.vehicle_type_id,
+                        modelSelect = $(tabId).find('.vehicle_model_select')[0],
+                        modelDataList = $(tabId).find('#vehicle_model')[0],
+                        label = $(tabId).find('.state-disabled')[0];
 
-               },
-               error: function(x, e) {
+                    $(label).removeClass('state-disabled');
+                    $(modelDataList).empty();
+                    $(modelSelect).prop('disabled', false);
+                    $(modelSelect).prop('value', null);
+                    if (modelData.length > 0) {
+                        for (i in modelData) {
+                            $(modelDataList).append("<option value='" + modelData[i]['name'] + "'>" + modelData[i]['name'] + "</option>");
+                        }
+                    }
+                },
+                error: function (x, e) {
 
-               }
+                }
 
-           });
-       }
-   });
+            });
+        }
+    });
 });
